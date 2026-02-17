08528c02267154f02293bb6ca69c6906fa097aef
shaheer-sa <chshaheer52@gmail.com>
2026-02-17 20:19:18 +0500

    Basic Class Structure

--- Q7.py
+++ Q7.py
@@ -1,3 +1,31 @@
-print("Hello World!")
+import matplotlib.pyplot as mat
+import collections
+import heapq
 
-print("hi")
+class AiPathfinder:
+    def __init__(self):
+        self.gridSize = 10
+        self.grid = [[0 for _ in range(self.gridSize)] for _ in range(self.gridSize)]
+        # Movement
+        self.moveOrder = [
+            (-1, 0),  # Up
+            (0, 1),   # Right
+            (1, 0),   # Bottom
+            (1, 1),   # Bottom-Right
+            (0, -1),  # Left
+            (-1, -1)  # Top-Left
+        ]
+
+        self.start = (9, 4)
+        self.target = (0, 6)
+        
+        # Walls
+        for r in range(2, 6):
+            self.grid[r][5] = -1
+
+    # check valid movement
+    def isValidMove(self, r, c):
+        if 0 <= r < self.gridSize and 0 <= c < self.gridSize:
+            if self.grid[r][c] != -1:
+                return True
+        return False
